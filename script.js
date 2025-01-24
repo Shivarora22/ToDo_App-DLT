803dc7fb3ea5266fef16330a4e59aac40238450c
Shiv Arora <shiv.arora@deeplogictech.com>
2025-01-24 14:11:27 +0530

    Added the Delete functionality in script.js

--- script.js
+++ script.js
@@ -11,8 +11,15 @@ document.addEventListener("DOMContentLoaded", () => {
         // Creating a new task here
         const listItem = document.createElement("li");
         listItem.textContent = task;
-  
         
+        // Added the  delete button
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.style.marginLeft = "10px";
+        deleteButton.addEventListener("click", () => {
+          taskList.removeChild(listItem);
+        });
+  
   
         listItem.appendChild(deleteButton);
         taskList.appendChild(listItem);
